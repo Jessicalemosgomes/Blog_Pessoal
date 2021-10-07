445fc7286f9d0a2a784a336430cf2d0dcf6be75f
jessica lemos gomes <lemosgomes24@gmail.com>
2021-10-06 22:14:34 -0300

    Adcionando camada de security e service

---
+++ blogPessoal/src/main/java/br/org/generation/blogpessoal/blogPessoalApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class blogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(blogPessoalApplication.class, args);
+	}
+
+}
